8294b4602769a1f917dce3a0da05fd680f1a2a41
Martin Jermar <mjermar@csas.cz>
2025-09-29 21:42:48 +0200

    update Exercise.cs

--- Workout.Domain/Entities/Exercise.cs
+++ Workout.Domain/Entities/Exercise.cs
@@ -8,39 +8,38 @@ public class Exercise : BaseEntity
 {
     public string Name { get; private set; }
     public string Description { get; private set; }
-    public string Category { get; private set; }
-    public Guid CreatedByUserId { get; private set; } // kdo cvik přidal
-    public MuscleGroup MuscleGroups { get; private set; } = new();
-    public List<EquipmentType> Equipment { get; private set; } = new();
-    public List<MediaItem> Media { get; private set; } = new();
+    public Guid CreatedByUserId { get; private set; }
+    public MuscleGroup PrimaryMuscleGroup { get; private set; }
+    public List<EquipmentType> Equipment { get; private set; }
+    public List<MediaItem> Media { get; private set; }
 
     private Exercise() { } // EF Core
 
-    public Exercise(string name, string description, string category,
+    public Exercise(
+        string name,
+        string description,
         Guid createdByUserId,
-        MuscleGroup muscleGroups,
-        IEnumerable<EquipmentType> equipment,
-        IEnumerable<MediaItem> media)
+        MuscleGroup primaryMuscleGroup,
+        IEnumerable<EquipmentType>? equipment,
+        IEnumerable<MediaItem>? media)
     {
         if (string.IsNullOrWhiteSpace(name))
             throw new DomainException("Exercise name cannot be empty.");
 
         Name = name;
         Description = description;
-        Category = category;
         CreatedByUserId = createdByUserId;
-        MuscleGroups = muscleGroups;
-        Equipment.AddRange(equipment);
-        Media.AddRange(media);
+        PrimaryMuscleGroup = primaryMuscleGroup;
+        Equipment = equipment?.ToList() ?? new List<EquipmentType>();
+        Media = media?.ToList() ?? new List<MediaItem>();
     }
 
-    // Metody pro editaci cviku (pokud uživatel smí upravovat)
-    public void Update(string? name, string? description, IEnumerable<MuscleGroup>? muscleGroups)
+    public void Update(string? name, string? description)
     {
-        if (!string.IsNullOrWhiteSpace(name)) Name = name;
-        if (!string.IsNullOrWhiteSpace(description)) Description = description;
-        
-        
-        
+        if (!string.IsNullOrWhiteSpace(name))
+            Name = name;
+
+        if (!string.IsNullOrWhiteSpace(description))
+            Description = description;
     }
 }
